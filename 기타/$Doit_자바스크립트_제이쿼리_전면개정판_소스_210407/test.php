0f4f3199f0bdca85d33c789e87c0f32558d19d7d
Dae-hong Lee <seumweb@seumweb.com>
2023-06-20 18:19:47 +0900

    230620 javaScript 134p까지 읽음

---
+++ 기타/$Doit_자바스크립트_제이쿼리_전면개정판_소스_210407/test.php
@@ -0,0 +1,26 @@
+<?php
+include_once("./_common.php");
+?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <!-- <link rel="stylesheet" href="css/ress.min.css"> -->
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <title>연습</title>
+</head>
+
+<body>
+    
+    <script>
+    var addNum = 0;
+    var auto = setTimeout(function() {
+        addNum++;
+        console.log(addNum);
+    }, 3000);
+    </script>
+
+</body>
+
+</html>
