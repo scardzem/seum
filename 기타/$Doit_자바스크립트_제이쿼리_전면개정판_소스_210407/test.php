2fa175d37e7f9bc38dd29b64299229b791572084
Dae-hong Lee <seumweb@seumweb.com>
2023-06-21 20:07:21 +0900

    js 책 199페이지 보는중.$.each() 메서드에서 막힘

--- 기타/$Doit_자바스크립트_제이쿼리_전면개정판_소스_210407/test.php
+++ 기타/$Doit_자바스크립트_제이쿼리_전면개정판_소스_210407/test.php
@@ -5,22 +5,45 @@ include_once("./_common.php");
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <!-- <link rel="stylesheet" href="css/ress.min.css"> -->
-    <!-- <link rel="stylesheet" href="css/style.css"> -->
-    <title>연습</title>
+<meta charset = "UTF-8">
+<title> 함수 </title>
+<script src="js/jquery.js"></script>
+<script>
+	$(function(){
+		var obj = [
+			{"area":"서울"},
+			{"area":"부산"},
+			{"area":"전주"}
+		];
+
+		$(obj).each(function(i, o){
+				console.log(i + ":", o);
+		});
+		console.log("==== The End 1 ====");
+
+		$.each($("#menu2 li"), function(i, o){
+				console.log(i + ":", o);
+		});
+		console.log("==== The End 2 ====");    
+
+		$.each($("#menu2 li"), function(i){
+				console.log(i + ":", $(this));
+		});    
+	});
+</script>
 </head>
-
 <body>
-    
-    <script>
-    var addNum = 0;
-    var auto = setTimeout(function() {
-        addNum++;
-        console.log(addNum);
-    }, 3000);
-    </script>
-
+	<h1>탐색 선택자</h1>
+	<ul id="menu1">
+		<li>내용1-1</li>
+		<li>내용1-2</li>
+		<li>내용1-3</li>
+	</ul>
+	<ul id="menu2">
+		<li>내용2-1</li>
+		<li>내용2-2</li>
+		<li>내용2-3</li>
+	</ul>
 </body>
 
 </html>
