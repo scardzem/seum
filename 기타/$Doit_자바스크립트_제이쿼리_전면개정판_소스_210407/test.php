adcbdc858875b1ec77a12061ee946cdf95e39b81
Dae-hong Lee <seumweb@seumweb.com>
2023-06-23 19:10:52 +0900

    책 283p까지 진도 나감

--- 기타/$Doit_자바스크립트_제이쿼리_전면개정판_소스_210407/test.php
+++ 기타/$Doit_자바스크립트_제이쿼리_전면개정판_소스_210407/test.php
@@ -9,51 +9,34 @@ include_once("./_common.php");
 <title> 함수 </title>
 <script src="js/jquery.js"></script>
 <script>
-	$(function(){
-		var obj = [
-			{"area":"서울"},
-			{"area":"부산"},
-			{"area":"전주"}
-		];
-
-		$(obj).each(function(i, o){
-				console.log("index=="+i + ":", o);  //,로 구분해서 출력(o는 객체이기 때문에 객체 정보를 출력하도록)
-		});
-		$(obj).each(function(i, o){
-				console.log("index=="+i + ":"+ o);  //이건 콘솔에 출력하면 그냥 object:object 이런식으로만 출력됨
-		});
-		$(obj).each(function(i, o){
-				console.log("index=="+i + ":" + o.area); // +는 문자열을 '연결'하는 기호라서 문자열이 아닌것에 쓰면
-		});											// index==0[object Object] 이런식으로 출력된다.
-													// 위의 식처럼 ,로 구분해야 객체로 출력이 된다.(콘솔창에서)
-													// +로 출력하고 싶으면 o.area로 출력해야 콘솔에 index==0:서울 이런식으로 출력된다.
-		//객체에 접근하는 방법 obj[0].area
-
-		console.log("==== The End 1 ====");
-
-		$.each($("#menu1 li"), function(i, o){
-				console.log(i + ":", o);
-		});
-		console.log("==== The End 2 ====");    
-
-		$.each($("#menu2 li"), function(i){
-				console.log(i + ":", $(this));
-		});    
+$(function(){
+	$(".btn_1").on("mouseover", function(){
+		alert("Hello");
 	});
+	$(document).on("mouseover",".btn_2", function(){
+		alert("Welcome");
+	});
+	//var btn_2 = $("<p><button class=\"btn_2\">버튼2</button></p>");  //대변의 $()이거 왜 쓰는거지? 무슨의미?
+	var btn_2 = "<p><button class=\"btn_2\">버튼2</button></p>";
+	$("#wrap").append(btn_2);
+	
+	$(".del_1").on("click", function(){
+		$(".btn_1").off("mouseover");
+	});
+	$(".del_2").on("click", function(){
+		$(document).off("mouseover", ".btn_2");
+	});
+});
 </script>
 </head>
 <body>
-	<h1>탐색 선택자</h1>
-	<ul id="menu1">
-		<li>내용1-1</li>
-		<li>내용1-2</li>
-		<li>내용1-3</li>
-	</ul>
-	<ul id="menu2">
-		<li>내용2-1</li>
-		<li>내용2-2</li>
-		<li>내용2-3</li>
-	</ul>
+<div id="wrap">
+	<p><button class="btn_1">버튼1</button></p>
+</div>
+<p>
+	<button class="del_1">버튼1 이벤트 해지</button> 
+	<button class="del_2">버튼2 이벤트 해지</button>
+</p>
 </body>
 
 </html>
