e9cb4194121b59480391512bb5cfce7ebc99d713
Dae-hong Lee <seumweb@seumweb.com>
2023-06-30 18:05:35 +0900

    230630 php 71번까지나감

--- 수습기간_제출과제/230712_php/ex.php
+++ 수습기간_제출과제/230712_php/ex.php
@@ -1,10 +1,8 @@
 <?php include_once("./_common.php"); ?>
 
 <?php
-    $str = "welcome to everdevel";
-
-    //변수 str에 있는 everdevel을 beancent로 치환
-    $changeWord = str_replace('everdevel','beancent',$str);
-    echo $changeWord;
+    include "php/70-1-include.php";
+    echo "<br>";
+    include_once "php/70-1-include.php";
 ?>
 
